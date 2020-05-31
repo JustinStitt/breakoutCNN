526a2b80e203ba9ee2fdc998d281b7bfa6c33658
Justin Stitt <cpt.crixus@gmail.com>
2020-05-30 21:07:23 -0700

    added project files and visuals

---
+++ breakout_main.py
@@ -0,0 +1,61 @@
+import gym
+import sys, os, time
+import numpy as np
+from DQN_CNN import Agent
+from matplotlib import pyplot
+from preprocessing import preprocess as pp
+
+render = False
+to_load = False
+to_save = True
+
+if __name__ == '__main__':
+    env = gym.make('Breakout-v0')
+
+    dims = pp(env.reset()).shape#take sample of environment and preprocess it to determine input's shape
+
+    agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 64,
+                    input_dims = dims, n_actions = env.action_space.n,  lr = 1e-4, eps_dec = 1e-4)
+    #observation space.shape = (210,160,3)
+    #pp observationspace.shape = (105,80,1)
+    if os.path.isfile('saved_models/saved_model_local.pt') and to_load:
+        agent.load_agent()
+        agent.epsilon = agent.eps_min
+
+    scores = []
+    epochs = 750
+    score = 0
+
+    for i in range(epochs):
+        if i % 1 == 0 and i > 0:
+            avg_score = np.mean(scores[-100:])#avg of last 100 scores
+            print('epoch: ', i, 'score: ', score, 'avg score: %.3f ' % avg_score,
+                        'epsilon: %.4f' % agent.epsilon)
+        if i % 50 == 0 and i > 0 and to_save:
+            agent.save_agent()
+
+        score = 0
+        observation = env.reset()
+        observation = pp(observation)
+        #print('-==OB SHAPE==-', observation.shape)
+        #new_ob = pp(observation)
+        #print('-=PP OBS=-', new_ob.shape)
+        #im_to_show = observation[::1,::1,-1]
+        #print('shown image shape: ', im_to_show.shape)
+        #pyplot.imshow(im_to_show, cmap='gray')
+        #pyplot.show()
+
+        done = False
+        while not done:
+            if render:
+                env.render()
+            action = agent.choose_action(observation)#adding to frames
+            observation_, reward, done, info = env.step(action)
+            observation_ = pp(observation_)
+            score += reward
+            agent.store_transition(observation, action, reward, observation_, done)#adding to frames
+            agent.learn()
+            observation = observation_
+        scores.append(score)
+    if to_save:
+        agent.save_agent()
--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 def to_grayscale(img):
-    return np.mean(img, axis = 2).astype(np.uint8)
+    gray =  np.mean(img, axis = 2).astype(np.uint8)#remove color
+    gray = gray.astype('float32')#normalize
+    gray /= 255. #normalize
+    return gray
 
 def downsample(img):
     return img[::2,::2]#reduce by factor of 2
 
 def preprocess(img):
-    return to_grayscale(downsample(img))#perform both preprocessing steps and return new img
+    _img =  to_grayscale(downsample(img))#perform both preprocessing steps and return new img
+    _img.shape = (_img.shape[0],_img.shape[1],1)
+    return _img
